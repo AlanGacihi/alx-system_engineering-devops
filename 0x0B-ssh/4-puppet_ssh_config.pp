d94083c9c41667fa39a666394c9cf8bbe7be48e4
AlanGacihi <alanmamicha@gmail.com>
2022-09-21 03:01:31 -0700

    Add Tasks

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# Changes the configuration of the ssh client.
+include stdlib
+file_line {'Turn off passwd auth':
+ensure => present,
+path   => '/etc/ssh/ssh_config',
+line   => 'PasswordAuthentication no',
+match  => '^PasswordAuthentication'
+}
+file_line {'Declare identity file':
+ensure => present,
+path   => '/etc/ssh/ssh_config',
+line   => 'IdentityFile ~/.ssh/holberton',
+}
